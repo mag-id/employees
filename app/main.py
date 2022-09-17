d601ddf5b8e4c0ccbc6ae3f081fa26e4da9b9d4f
mag-id <>
2022-09-18 02:21:24 +0300

    add main

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI, Depends
+from app.database import find_employees
+from app.models import Employee
+
+APP = FastAPI()
+
+
+@APP.get("/employees", response_model=list[Employee])
+async def employees(query: Employee = Depends()) -> list[Employee]:
+    return find_employees(query)
